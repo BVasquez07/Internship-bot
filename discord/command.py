57fe4b57cc27f9b431868e4ba733d3534bfe681c
IshmamF <ishmamf2003@gmail.com>
2023-12-31 20:34:01 -0500

    created separate file to store functions

--- discord/command.py
+++ discord/command.py
@@ -1,5 +1,4 @@
-import requests
-import random
+from utils import search_active, search_location, search_sponsorship, create_embed, internFilter
 import os
 import asyncio
 from discord.ext import commands
@@ -9,30 +8,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-MAIN_URL = "https://raw.githubusercontent.com/SimplifyJobs/Summer2024-Internships/dev/.github/scripts/listings.json"
 
 intents = Intents.default()
 intents.message_content = True
 
 bot = commands.Bot(command_prefix='$', intents=intents)
-
-def search_location(location, count):
-    response = requests.get(MAIN_URL)
-    internships = response.json()
-    active_internships = [internship for internship in internships if any(location.lower() in loc.lower() for loc in internship['locations']) and internship['active']]
-    return random.sample(active_internships, min(len(active_internships), count))
-
-def search_sponsorship(count):
-    internships = requests.get(MAIN_URL).json()
-    active_internships = [internship for internship in internships if "Offers Sponsorship" == internship['sponsorship'] and internship['active']]
-
-    return random.sample(active_internships, count)
-
-def search_active(count):
-    internships = requests.get(MAIN_URL).json()
-    active_internships = [internship for internship in internships if internship['active']]
-
-    return random.sample(active_internships, count)
 
 @bot.command(name='find')
 async def find_internships(ctx, *args):
@@ -46,26 +26,12 @@ async def find_internships(ctx, *args):
         await ctx.send("You didn't reply in time, please try the command again.")
         return
     count = int(message.content)
-    if search_query == 'sponsorship':
-        internships = search_sponsorship(count)
-    elif search_query == 'active':
-        internships = search_active(count)
-    else:
-        internships = search_location(search_query, count)
+    internships = internFilter(search_query, count)
     if not internships:
         await ctx.send(f"No internships found for {search_query}.")
         return
     page = 0
     max_page = len(internships) - 1
-    def create_embed(internship, page, max_page):
-        embed = Embed(title=internship['title'],
-                      description=f"**Company:** {internship['company_name']}\n"
-                                  f"**Location:** {', '.join(internship['locations'])}\n"
-                                  f"**Sponsorship:** {internship['sponsorship']}",
-                      color=0xcd4fff)
-        embed.add_field(name="Apply", value=f"[Click here]({internship['url']})", inline=False)
-        embed.set_footer(text=f"Page {page+1} of {max_page+1}")
-        return embed
     view = View()
     previous_button = Button(style=ButtonStyle.primary, label='Previous', disabled=True)
     next_button = Button(style=ButtonStyle.primary, label='Next', disabled=(max_page == 0))
---
+++ discord/utils.py
@@ -0,0 +1,41 @@
+import requests
+import random
+from discord import Embed
+
+MAIN_URL = "https://raw.githubusercontent.com/SimplifyJobs/Summer2024-Internships/dev/.github/scripts/listings.json"
+
+def search_location(location, count):
+    response = requests.get(MAIN_URL)
+    internships = response.json()
+    active_internships = [internship for internship in internships if any(location.lower() in loc.lower() for loc in internship['locations']) and internship['active']]
+    return random.sample(active_internships, min(len(active_internships), count))
+
+def search_sponsorship(count):
+    internships = requests.get(MAIN_URL).json()
+    active_internships = [internship for internship in internships if "Offers Sponsorship" == internship['sponsorship'] and internship['active']]
+
+    return random.sample(active_internships, count)
+
+def search_active(count):
+    internships = requests.get(MAIN_URL).json()
+    active_internships = [internship for internship in internships if internship['active']]
+
+    return random.sample(active_internships, count)
+
+def create_embed(internship, page, max_page):
+    embed = Embed(title=internship['title'],
+                    description=f"**Company:** {internship['company_name']}\n"
+                                f"**Location:** {', '.join(internship['locations'])}\n"
+                                f"**Sponsorship:** {internship['sponsorship']}",
+                    color=0xcd4fff)
+    embed.add_field(name="Apply", value=f"[Click here]({internship['url']})", inline=False)
+    embed.set_footer(text=f"Page {page+1} of {max_page+1}")
+    return embed
+
+def internFilter(search_query, count):
+    if search_query == 'sponsorship':
+        return search_sponsorship(count)
+    elif search_query == 'active':
+        return search_active(count)
+    else:
+        return search_location(search_query, count)
